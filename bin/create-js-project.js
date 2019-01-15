5bb120b2ba0ac94d7ce4fa2669bf09a5dbc28196
Angelos Chalaris <chalarangelo@gmail.com>
2019-01-15 21:54:34 +0200

    Switched to template system

--- bin/create-js-project.js
+++ bin/create-js-project.js
@@ -2,18 +2,36 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
-const argv = require('minimist')(process.argv.slice(2));
-const packageQuestions = require('../lib/questions/packageQuestions')(argv, process.cwd());
-const buildPackage = require('../lib/scripts/buildPackage');
+const { readTemplates } = require('../lib/templates.js');
+const { group } = require('../lib/util.js');
+const { buildPackage } = require('../lib/buildPackage');
 
-inquirer.prompt([...packageQuestions]).then(answers => {
+const argv = require('minimist')(process.argv.slice(2));
+const templatesData = readTemplates(__dirname);
+const templates = {
+  values: group(templatesData, v => v.category).reduce((acc, v) => [...acc, new inquirer.Separator(), ...v])
+};
+templates.default = templates.values.indexOf('JavaScript');
+
+const questionOptions = {
+  params: argv,
+  currentDir: process.cwd(),
+  templates: templates
+}
+
+const questions = require('../lib/questions')(questionOptions);
+
+inquirer.prompt(questions).then(answers => {
+  console.log(JSON.stringify(answers, null, 2));
+
   if (answers.overwriteDir !== undefined && !answers.overwriteDir) {
     console.log('Please try again with a different name!');
     process.exit(0);
   }
-
-  if (answers.overwriteDir === undefined) fs.mkdirSync(`${answers.packageName}`);
-  process.chdir(`./${answers.packageName}`);
+  if (answers.overwriteDir === undefined) 
+    fs.mkdirSync(`${answers.projectName}`);
+  
+    process.chdir(`./${answers.projectName}`);
 
   fs.writeFileSync('package.json', buildPackage(answers));
 });
