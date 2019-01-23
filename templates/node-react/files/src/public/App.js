21f48ef39e24ca77b89753e9b086d36cdc0ae4d1
Angelos Chalaris <chalarangelo@gmail.com>
2019-01-23 18:31:05 +0200

    Add react fullstack templates (node and express)

---
+++ templates/node-react/files/src/public/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>A React app</h1>
+        <p>Edit <code>src/App.js</code>, save and reload.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
